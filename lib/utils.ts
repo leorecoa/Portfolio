c6630f71933d935ed9bf74527010a49c068d92f7
leorecoa <leorecoa2@gmail.com>
2026-01-25 04:12:21 -0300

    refactor: reestrutura arquitetura de pastas e remove arquivos redundantes
    - Move componentes de seção para 'components/sections/'
    - Move efeitos visuais para 'components/effects/'
    - Centraliza constantes em 'lib/constants.tsx'
    - Adiciona utilitário 'cn' em 'lib/utils.ts'
    - Remove arquivos wrapper da raiz (Hero.tsx, About.tsx, etc.)
    - Atualiza importações no App.tsx para refletir a nova estrutura

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,7 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...classes: (string | boolean | undefined)[]) {
-  return classes.filter(Boolean).join(' ');
+// Função utilitária para mesclar classes do Tailwind sem conflitos
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
